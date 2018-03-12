2b2df39e499e033d1cd2a297a6d9908c4d3e67bd
Viktor <viktorsoderstrom@msn.com>
2018-03-12 17:03:59 +0100

    added Tippa component

---
+++ client/src/components/Tippa.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Tippa</h1>
+    <div>{{ rader }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      rader: {}
+    }
+  },
+  methods: {
+    getRows () {
+
+    }
+  },
+  created: function () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
